123f39b17d3ab09d622c39cb5399eb0e6decd34f
luhuifang <luhuifang@genomics.cn>
2024-07-23 10:47:42 +0800

    update: 202407231047

---
+++ docs/gitbook/gitbook-plugin-anchor-to-full-links/plugin.js
@@ -0,0 +1,24 @@
+
+require(['gitbook', 'jquery'], function(gitbook, $) {
+    var opts;
+
+    gitbook.events.bind('start', function(e, config) {
+        opts = config['anchor-to-full-links'].elements;
+    });
+
+    gitbook.events.bind('page.change', function() {
+        const a = window.location.href.replace(/\/([^\/]*)$/, '/')
+        $.map(opts, function(ele) {
+            $(ele).each((index, value) => {
+                if(value.attributes.getNamedItem('href')){
+                    const href = value.attributes.getNamedItem('href').value
+                    
+                    if(href.match(/.*\.html#.*/)){
+                        value.attributes.getNamedItem('href').value = a + href
+                    }
+                    
+                }
+            });
+        });
+    });
+});
